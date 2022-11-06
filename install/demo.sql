b8596a668fb224cf373aaf721db488ba59426184
Alexander Walther <aw@alexplus.de>
2022-11-06 22:03:44 +0100

    demo sql

---
+++ install/demo.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8mb4;
+
+INSERT INTO `rex_yform_email_template` (`name`, `mail_from`, `mail_from_name`, `mail_reply_to`, `mail_reply_to_name`, `subject`, `body`, `body_html`, `attachments`, `updatedate`) VALUES
+('stellenangebote_apply_confirm',	'',	'',	'',	'',	'',	'',	'',	'',	NOW()),
+('stellenangebote_apply',	'',	'',	'',	'',	'',	'',	'',	'',	NOW());
+
+INSERT INTO `rex_stellenangebote_contact` (`status`, `name`, `mail`, `phone`, `description`) VALUES
+('1',	'Personalabteilung',	'bewerbung@example.org',	'012345 / 6789 - 0',	'<p></p>');
+
+INSERT INTO `rex_stellenangebote_location` (`google_places`, `name`, `street`, `zip`, `locality`, `countrycode`, `lat_lng`, `lat`, `lng`) VALUES
+('',	'Unternehmensname',	'',	'',	'',	'DE',	'',	'');
--- pages/settings.php
+++ pages/settings.php
@@ -41,10 +41,6 @@ $fragment->setVar('title', $this->i18n('stellenangebote_donate'), false);
 $fragment->setVar('body', '<p>' . $this->i18n('stellenangebote_info_donate') . '</p>' . $anchor, false);
 echo !rex_config::get("alexplusde", "donated") ? $fragment->parse('core/page/section.php') : "";
 
-if (rex_addon::get('url')->isAvailable()) {
-    echo rex_view::info($this->i18n('stellenangebote_info_url_active'));
-}
-
 $package = rex_install_packages::getUpdatePackages();
 if (isset($packages['stellenangebote'])) {
     $current_version = rex_addon::get('stellenangebote')->getProperty('version');
@@ -56,6 +52,28 @@ if (isset($packages['stellenangebote'])) {
     };
 };
 
+$func = rex_request('func', 'string');
+$csrf = rex_csrf_token::factory('stellenangebote_demo');
+if ($func !== '') {
+    if (!$csrf->isValid()) {
+        echo rex_view::error(rex_i18n::msg('csrf_token_invalid'));
+    } else {
+        if ($func === 'setup') {
+            $file = rex_path::addon('stellenangebote').'install/demo.sql';
+            rex_sql_util::importDump($file);
+            echo rex_view::success(rex_i18n::msg('stellenangebote_demo_imported'));
+        }
+    }
+}
+$content = "";
+$content .= '<p>'.rex_i18n::msg('Demo-Daten imporiteren').'</p>';
+$content .= '<p><a class="btn btn-primary" href="'.rex_url::currentBackendPage(['func' => 'setup'] + $csrf->getUrlParams()).'" data-confirm="'.rex_i18n::msg('stellenangebote_demo_warning').'">'.rex_i18n::msg('stellenangebote_demo').'</a></p>';
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'danger', false);
+$fragment->setVar('title', rex_i18n::msg('stellenangebote_demo'), false);
+$fragment->setVar('body', $content, false);
+echo $fragment->parse('core/page/section.php');
 ?>
 	</div>
 </div>
