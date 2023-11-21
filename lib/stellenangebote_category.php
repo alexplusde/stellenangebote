b72d23673fa7a5744668273d51ff7ea0525b97df
Paul Götz <20090597+schorschy@users.noreply.github.com>
2023-11-21 15:34:57 +0100

    Update stellenangebote_category.php

--- lib/stellenangebote_category.php
+++ lib/stellenangebote_category.php
@@ -1,26 +1,33 @@
 <?php
+
 class stellenangebote_category extends \rex_yform_manager_dataset
 {
-
-    public function getName() :string
+    public function getName(): string
     {
         return $this->getValue('name');
     }
-    public function getDescription() :string
+    public function getDescription(): string
     {
         return $this->getValue('description');
     }
-    public function getIcon() :string
+    public function getIcon(): string
     {
         return $this->getValue('icon');
     }
-    public function getStatus() :string
+    public function getStatus(): string
     {
         return $this->getValue('status');
     }
-    public function getImage() :string
+    public function getImage(): string
     {
         return $this->getValue('image');
+    }
+    public function getMedia(): rex_media
+    {
+        if($media = rex_media::get($this->getValue('image'))) {
+            return $media;
+        }
+        return "no media with such filename found";
     }
 
 }
