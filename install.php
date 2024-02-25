2295236511a270de3df71d3346c9aa760fcf703d
Alexander Walther <aw@alexplus.de>
2024-02-25 17:45:05 +0100

    bs5 optional machen

--- install.php
+++ install.php
@@ -1,6 +1,8 @@
 <?php
 
+if (rex_addon::get('plus_bs5')->isAvailable() && !rex::isSafeMode()) {
 bs5::forceBackup();
+}
 
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_stellenangebote.tableset.json')));
@@ -12,6 +14,7 @@ if (rex_config::get('stellenangebote', 'company_name') === "") {
     rex_config::set('stellenangebote', 'company_url', rex_article::getSiteStartArticleId());
 }
 
-
-bs5::updateModule("stellenangebote");
-bs5::updateTemplate("stellenangebote");
+if (rex_addon::get('plus_bs5')->isAvailable() && !rex::isSafeMode()) {
+    bs5::updateModule("stellenangebote");
+    bs5::updateTemplate("stellenangebote");
+}
